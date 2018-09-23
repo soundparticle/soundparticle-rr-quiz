bb991574bd84b5cfeb669931a1e44e088e74260c
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-23 15:04:00 -0700

    load colors reducer test passing

--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -11,4 +11,15 @@ describe('Color reducers', () => {
     const state = colors(undefined, {});
     expect(state).toEqual([]);
   });
+
+  it('loads colors', () => {
+    const payload = [{}, {}, {}];
+
+    const state = colors([], {
+      type: COLORS_LOAD,
+      payload
+    });
+
+    expect(state).toBe(payload);
+  });
 });
